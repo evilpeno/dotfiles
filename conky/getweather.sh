fb9b668330d17211fcdebc8ed979a7bb18bf239a
Nick Brooks <nibrooks@gmail.com>
2017-07-23 16:19:41 -0600

    Updating for my location

--- conky/getweather.sh
+++ conky/getweather.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!rm -f ~/.cache/weather.json
-curl -s "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Passo%20Fundo%2C%20BR%22)%20and%20u=%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" -o ~/.cache/weather.json
-#!sleep 4
+rm -f ~/.cache/weather.json
+curl -s "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22Salt%20Lake%2City%20UT%22)%20and%20u=%27f%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" -o ~/.cache/weather.json
